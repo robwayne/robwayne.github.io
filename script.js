d6069cc700aa2ca5e2d42f0b6f68d13d825fe044
robwayne <robwaynegordon@gmail.com>
2018-02-27 17:19:41 +0400

    Made all the project cards links

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 $(document).ready(() => {
-
-
   //create a list of links on the page. Get the corresponding link in the array when a project is clicked and navigate to that page
-  let links = ['30MFF/index.html', 'forty-acres/index.html']
+  let links = ['30MFF/index.html', 'forty-acres/index.html'];
   let linkIndex = -1;
   //getting a list of all the projects on the page to loop over them later.
-  let projects = $(".project");
-  projects.each((index) => {
-    //console.log(index);
-    linkIndex = index;
-    console.log(linkIndex);
-    $(this).click(() =>{
-      if (linkIndex !== -1){
-        console.log(linkIndex);
-        //window.location.href = links[linkIndex];
-      }
-    });
-  });
+  
 
 });
