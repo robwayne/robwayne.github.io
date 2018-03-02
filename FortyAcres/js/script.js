dd0505c9820497cadd11f433b6fdbe584b92f570
robwayne <robwaynegordon@gmail.com>
2018-03-02 16:03:58 +0400

    Added description bar at bottom:

--- FortyAcres/js/script.js
+++ FortyAcres/js/script.js
@@ -12,7 +12,6 @@ preload = () => {
 let fft,y, w,x; //create all globabl variables
 setup = () => {
 
-  colorMode(HSB); //set the color mode to hsb to get better colors
   fft = new p5.FFT(0.9,512); // create a new p5.FFT object to analyze the audio data - apply smoothing and create 1024 bins
 
   //Declare all the necessary variables for the skecth to create the wavy pattern.
@@ -24,7 +23,6 @@ setup = () => {
   let titleDiv = createP("forty acres");
 
   titleDiv.class("title");
-
   overlayDiv.class("overlay");
   headerDiv.class("header");
   headerDiv.id("header");
@@ -50,27 +48,21 @@ let audioLength;
 draw = () => {
 
   audioLength = sound.duration();
-
   background(img);
+
   soundSpectrum = fft.analyze();
   for (let i = 0; i < soundSpectrum.length; i++){
     y = map(soundSpectrum[i], 0,256, height/2,0);
     x = map(i,0,soundSpectrum.length, 0,width/2);
-    green = map(i,0, soundSpectrum.length,5,10);
     noStroke();
-    fill(255,green,255);
+    fill(255,255,255);
     rect((width/2)-x,y,w-2,(height/2)-y);
-    stroke(255);
     noStroke();
-    fill(255,green,255);
     rect((width/2)-x,height/2, w-2,(height/2)-y);
 
     noStroke();
-    fill(255,green,255);
     rect(x+(width/2),y,w-2,(height/2)-y);
-    stroke(255);
     noStroke();
-    fill(255,green,255);
     rect(x+(width/2),height/2, w-2,(height/2)-y);
 
   }
@@ -92,6 +84,11 @@ draw = () => {
   currentTime = sound.currentTime();
   if(currentTime >= audioLength-0.2){
     $("#playbutton").html('<i class="fas fa-redo"></i>');
+  }
+  if (sound.isPlaying()){
+    $("#description").fadeOut(500);
+  }else{
+    $("#description").fadeIn(500);
   }
 }
 
